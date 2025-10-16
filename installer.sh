80c870fe4eb5bdd599b85d7fc48b3ba98ece8205
VC365 <xdood.sav@outlook.com>
2025-10-16 13:31:11 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,34 +1,51 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
-DIR_APP="/usr/bin"
-install() {
-    echo -e "${YELLOW}installing vc-clock...${NC}"
-    if [ ! -f vc-clock ]; then
-        echo -e "${RED} vc-clock not found in current directory!${NC}"
-        exit 1
-    fi
 
-	  if [ -f "$DIR_APP/vc-clock" ]; then
-	      sudo rm "$DIR_APP/vc-clock"
-  	fi
-	  chmod a+x vc-clock
-	  sudo cp vc-clock "$DIR_APP"
-  	echo -e "${GREEN} vc-clock Installed!${NC}"
+SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
+name="vc-clock"
+
+install_c()
+{
+  echo -e "${YELLOW} $name Installing...${NC}"
+  if [ ! -f "$SCRIPT_DIR/bin/c/$name" ]; then
+    make c || { echo "${RED}Build failed${NC}"; exit 1; }
+  fi
+  sudo install -m 755 "$SCRIPT_DIR/bin/c/$name" "/usr/bin/"
+  echo -e "${GREEN} $name Installed!${NC}"
 }
-
+install_crystal()
+{
+  echo -e "${YELLOW} $name Installing...${NC}"
+  if [ ! -f "$SCRIPT_DIR/bin/$name" ]; then
+    make crystal || { echo "${RED}Build failed${NC}"; exit 1; }
+  fi
+  sudo install -m 755 "$SCRIPT_DIR/bin/$name" "/usr/bin/"
+  echo -e "${GREEN} $name Installed!${NC}"
+}
 uninstall() {
-    sudo rm -f "$DIR_APP/vc-clock" &> /dev/null
-    echo -e "${GREEN} vc-clock Deleted!${NC}"
+    sudo rm -f "/usr/bin/$name" &> /dev/null
+    echo -e "${RED} $name Deleted!${NC}"
 }
-
-if [ "$1" == "install" ]; then
-    install
-elif [ "$1" == "uninstall" ]; then
-    uninstall
-else
-    echo -e "${RED} Options $0 {install|uninstall}${NC}"
-    exit 1
-fi
+main()
+{
+  if [ "$1" == "install" ]; then
+      if [ "$2" == "crystal" ]; then
+        install_crystal
+      elif [ "$2" == "c" ]; then
+        install_c
+      else
+        echo -e "${RED}Usage:${NC} $0 install {c|crystal}"
+        exit 1
+      fi
+  elif [ "$1" == "uninstall" ]; then
+      uninstall
+  else
+      echo -e "${RED} Options $0 { install | uninstall }${NC}"
+      exit 1
+  fi
+}
+main "$@"
